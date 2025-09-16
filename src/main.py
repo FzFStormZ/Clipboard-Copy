0319eeff36ecf47edd65d135a6bc8e9f6dc422b8
FzFStormZ <dylan.degail@gmail.com>
2025-09-16 22:56:11 +0200

    Add Python files + requirements

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from clipboard import xray
+
+def main():
+    print("Checking if there is a current password inside the clipboard...")
+    # Run the checking
+    # If found, print it
+    # Else
+    print("XRay activated... Be careful to not put your password inside the clipboard :)")
+    # while 
+    # Print if a new password is detected !
+    # Create at the end of the program a list of all the passwords founded and put it inside a wordlist 
+
+
+
+
+if __name__ == "__main__":
+    main()
