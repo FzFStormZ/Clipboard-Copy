ff8a14a0b46102b3099e5ab8459caf97524cbb21
FzFStormZ <dylan.degail@gmail.com>
2025-09-17 19:13:00 +0200

    Writing code

--- src/clipboard/xray.py
+++ src/clipboard/xray.py
@@ -1,11 +1,25 @@
 import pyperclip as clip
 from utils import utils
+from pynput import keyboard
 
 class XRay:
     def __init__(self, nb_caracs: int = 8, specials: bool = True, numbers: bool = True) -> None:
-        self.passwords = []
+        self.passwords = set()
         self.conditions = {'nb_caracs': nb_caracs, 'specials': specials, 'numbers': numbers}
+        self.keyboard = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
 
+    def on_press(key) -> None:
+        print("test")
+        if key == keyboard.Key.enter:
+            # Stop listener
+            return False
+        pass
+
+    def on_release(key) -> None:
+        print("test")
+        pass
+
+    # Check if the given text is actually a password according to the conditions of the instance
     def is_password(self, text: str) -> bool:
         nb = len(text) > self.conditions['nb_caracs']
         special = utils.has_special_chars(text)
@@ -15,14 +29,30 @@ class XRay:
     def read_current_data(self) -> None:
         text = clip.paste()
         if self.is_password(text):
-            self.passwords.append(text)
-            print(f"Password founded: {text}")
+            self.passwords.add(text)
+            print(f"Password founded: {text}!")
+            print(f"Added to the passwords list")
         else:
             print("Password not found :)")
 
     def read_until_password(self) -> None:
-        clip.paste
-        pass
+        # init variables
+        text = ''
+        password = ''
 
+        # start the keyboard listener thread
+        self.keyboard.start()
+
+        while self.keyboard.is_alive():
+            print("XRay activated... [CTRL+C] to stop the listening")
+            text = clip.paste()
+            if self.is_password(text) and password != text:
+                password = text
+                print(f"Password founded: {password}!")
+                self.passwords.add(password)
+                print(f"Added to the passwords list")
+        print("Stopping the XRay...")
+                
+    # Put all the founded passwords inside a wordlist
     def export_passwords(self, file) -> None:
         pass
--- src/main.py
+++ src/main.py
@@ -3,16 +3,8 @@ from clipboard import xray
 def main():
     print("Checking if there is a current password inside the clipboard...")
     test = xray.XRay()
-    test.read_current_data()
-    # Run the checking
-    # If found, print it
-    # Else
-    print("XRay activated... Be careful to not put your password inside the clipboard :)")
-    # while 
-    # Print if a new password is detected !
+    test.read_until_password() # Hello12@AdffrEZZ
     # Create at the end of the program a list of all the passwords founded and put it inside a wordlist 
-
-
 
 
 if __name__ == "__main__":
